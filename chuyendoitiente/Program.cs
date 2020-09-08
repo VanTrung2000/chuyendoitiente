3e2dd058be06e66cdf44dde50a3b03e8f52545ef
VanTrung2000 <vantrungdeptrai2000@gmail.com>
2020-09-08 22:16:21 +0700

    Add project files.

---
+++ chuyendoitiente/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace chuyendoitiente
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            int tigia = 23000;
+            Console.WriteLine("Nhap USD :");
+            int usd = int.Parse(Console.ReadLine());
+            int vnd = tigia * usd;
+            Console.WriteLine("VNĐ là : {0} Vnđ", vnd);
+            Console.ReadKey();
+        }
+    }
+}
