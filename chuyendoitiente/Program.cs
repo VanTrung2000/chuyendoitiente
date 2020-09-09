4ecdb8d8a061ca87e364af0bc7dc89d078268b41
VanTrung2000 <70925652+VanTrung2000@users.noreply.github.com>
2020-09-09 19:20:07 +0700

    Update Program.cs

--- chuyendoitiente/Program.cs
+++ chuyendoitiente/Program.cs
@@ -1,19 +1,24 @@
-﻿using System;
+using System;
+using System.Xml.Serialization;
 
 namespace chuyendoitiente
 {
     class Program
-    {
+    {             
         static void Main(string[] args)
         {
-
-
-            int tigia = 23000;
+            int usd, tigia = 23000000;
             Console.WriteLine("Nhap USD :");
-            int usd = int.Parse(Console.ReadLine());
-            int vnd = tigia * usd;
-            Console.WriteLine("VNĐ là : {0} Vnđ", vnd);
+            usd = int.Parse(Console.ReadLine());
+            vnd(usd, tigia);
             Console.ReadKey();
         }
+        static int vnd (int a,int b)
+        {
+            int c = a * b;
+            Console.WriteLine("VNĐ là : {0} Vnđ", +c);
+            return c;
+        }
+
     }
 }
